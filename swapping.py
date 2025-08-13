c870493be4747c69b32c2910d128136b6ede3659
Vikkycodees <rama198283@gmail.com>
2025-08-14 00:09:52 +0530

    added pattern and added swapping

---
+++ swapping.py
@@ -0,0 +1,14 @@
+# Pythonic swap using tuple unpacking
+a = 10
+b = 20
+
+print("Before swapping:")
+print("a =", a)
+print("b =", b)
+
+# Swap happens here
+a, b = b, a
+
+print("\nAfter swapping:")
+print("a =", a)
+print("b =", b)
